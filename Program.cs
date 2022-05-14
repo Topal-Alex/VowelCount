ce316422b2c275f4eae05138c23e40ff52ab03c6
Alex <79322837+Topal-Alex@users.noreply.github.com>
2022-05-14 11:49:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+
+namespace ConsoleApplication1
+{
+    class  Program
+    {
+        static void Main(string[] args)
+        {
+            GetVowelCount("good job")
+
+        }
+        public static int GetVowelCount(string str)
+        {
+            int vowelCount = 0;
+            string Glas = "aeiou";
+            for(int i = 0;i<str.Length;i++)
+            {
+                for(int j = 0;j< Glas.Length;j++)
+                {
+                    if(str[i]==Glas[j])
+                    {
+                        vowelCount++;
+                    }
+                }
+            }
+        
+
+            return vowelCount;
+        }
+
+        
+
+
+    }
+    
+    
+}
